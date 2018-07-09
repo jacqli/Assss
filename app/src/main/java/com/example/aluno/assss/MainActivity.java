3c716a6a0f2917c8cfabc56a8bfcec4803db4b05
aluno <alpaca121@hotmail.com>
2018-07-09 10:14:47 -0300

    Eu dsou gay

---
+++ app/src/main/java/com/example/aluno/assss/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.aluno.assss;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+private Button entrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        entrar = (Button) findViewById(R.id.bEntrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this , Tela2Activity.class));
+            }
+        });
+    }
+}
